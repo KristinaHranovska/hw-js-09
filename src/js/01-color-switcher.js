6b8dd6d5af5e80b27df53449b71d007199a3a44c
Kristina <kr.hranovska@gmail.com>
2023-11-18 17:30:17 +0200

    first task is ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let randomStylePage;
 
+btnStart.addEventListener('click', startRandom);
+btnStop.addEventListener('click', stopRandom);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function startRandom() {
+    randomStylePage = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        btnStart.disabled = true;
+        btnStop.disabled = false;
+    }, 1000);
+}
+
+function stopRandom() {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+    clearInterval(randomStylePage);
+}
